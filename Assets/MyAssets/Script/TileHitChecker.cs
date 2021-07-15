4e856479985fbe750737d3b4a87949d095dca69a
yano-hitoshi <yano.hitoshi.1993@gmail.com>
2021-07-15 17:11:30 +0900

    瓦がプレイヤーと当たった時に割れる処理

---
+++ Assets/MyAssets/Script/TileHitChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Behaviors;
+using TouchScript.Gestures.TransformGestures;
+
+/// <summary>
+/// 瓦の当たり判定をチェックするクラス
+/// </summary>
+public class TileHitChecker : MonoBehaviour
+{
+    // 割れていない瓦
+    [SerializeField] GameObject tileObject;
+    // 割れている瓦
+    [SerializeField] GameObject breakTileObject;
+
+    /// <summary>
+    /// コライダーに触れた時
+    /// </summary>
+    /// <param name="collision">触れたオブジェクトのコライダー</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<ScreenTransformGesture>().enabled = false;
+        collision.gameObject.GetComponent<Transformer>().enabled = false;
+        collision.rigidbody.isKinematic = true;
+        tileObject.SetActive(false);
+        breakTileObject.SetActive(true);
+    }
+
+}
