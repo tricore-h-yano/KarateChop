f8a86bcc3bd431ed4a6343555e71848a24aac667
yano-hitoshi <yano.hitoshi.1993@gmail.com>
2021-08-02 18:32:06 +0900

    一時保存

---
+++ Assets/MyAssets/Script/StartButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButtonController : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] TileTypeSelector tileTypeSelector;
+    [SerializeField] GameObject fadeInOutObject;
+    [SerializeField] GameObject nowScene;
+    [SerializeField] GameObject nextScene;
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    void Start()
+    {
+        button.onClick.AddListener(Transition);
+    }
+
+    void Transition()
+    {
+        StartCoroutine("FadeInOut");
+    }
+
+    IEnumerator FadeInOut()
+    {
+        tileTypeSelector.SelectColor();
+        fadeInOutObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        nowScene.SetActive(false);
+        nextScene.SetActive(true);
+        fadeInOutObject.SetActive(false);
+    }
+}
