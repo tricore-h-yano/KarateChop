def1165a65f3e70ac0d7f7e952d2d15405bed51e
yano-hitoshi <yano.hitoshi.1993@gmail.com>
2021-08-03 10:53:23 +0900

    保存

---
+++ Assets/MyAssets/Script/TutorialButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialButtonController : MonoBehaviour
+{
+    [SerializeField] Button button = default;
+    [SerializeField] Animator animator = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(() =>OnClickTransition());
+    }
+
+    void OnClickTransition()
+    {
+        animator.SetBool("TitleToTutorial", true);
+    }
+}
